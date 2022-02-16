646afcf290bcc9212135e42a92417d4222e7e717
Rhys Huxley <rhyshuxley@gmail.com>
2022-02-16 19:36:21 +0000

    Finish profile page

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -19,6 +19,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/navbar.css') }}" rel="stylesheet">    
+    <link href="{{ asset('css/navbar.css') }}" rel="stylesheet"> 
+    <link href="{{ asset('css/profile.css') }}" rel="stylesheet"> 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" rel="stylesheet">
 </head>

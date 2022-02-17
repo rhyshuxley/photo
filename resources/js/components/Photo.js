14c1abc4514882530e9bd4aabc3a40a8d221e193
Rhys Huxley <rhyshuxley@gmail.com>
2022-02-17 00:55:33 +0000

    Add Photo component

--- resources/js/components/Photo.js
+++ resources/js/components/Photo.js
@@ -1,13 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../../../public/css/photos.css';
 
 function Photo() {
+    let data = [
+        {
+            id: 1,
+            imgSrc: 'https://source.unsplash.com/random',
+        },
+        {
+            id: 2,
+            imgSrc: 'https://source.unsplash.com/random',
+        },
+        {
+            id: 3,
+            imgSrc: 'https://source.unsplash.com/random',
+        },
+        {
+            id: 4,
+            imgSrc: 'https://source.unsplash.com/random',
+        },
+        {
+            id: 5,
+            imgSrc: 'https://source.unsplash.com/random',
+        },
+        {
+            id: 6,
+            imgSrc: 'https://source.unsplash.com/random',
+        },
+        {
+            id: 7,
+            imgSrc: 'https://source.unsplash.com/random',
+        },
+        {
+            id: 8,
+            imgSrc: 'https://source.unsplash.com/random',
+        },
+        {
+            id: 9,
+            imgSrc: 'https://source.unsplash.com/random',
+        },
+        {
+            id: 10,
+            imgSrc: 'https://source.unsplash.com/random',
+        },
+        {
+            id: 11,
+            imgSrc: 'https://source.unsplash.com/random',
+        },
+        {
+            id: 12,
+            imgSrc: 'https://source.unsplash.com/random',
+        }
+    ]
+
     return (
-        <div className="col">
-            <div className="card">
-                <div className="card-header">Photo Component</div>
-                <div className="card-body">Photo Gallery goes here!</div>
-            </div>
+        <div className='gallery'>
+            {data.map((item, index) => {
+                return (
+                    <div className="photo" key={index}>
+                        <img src={item.imgSrc}></img>
+                    </div>
+                )
+            })}
         </div>
     );
 }

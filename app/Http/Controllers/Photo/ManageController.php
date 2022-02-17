2ec3103c4a6232ce5005280c26b195f419a21278
Rhys Huxley <rhyshuxley@gmail.com>
2022-02-17 18:48:59 +0000

    Update import

--- app/Http/Controllers/Photo/ManageController.php
+++ app/Http/Controllers/Photo/ManageController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Photo;
 
 
 use App\Http\Controllers\Controller;
-use App\RHP\Photos\Photo;
+use App\Models\Photo\Photo;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;

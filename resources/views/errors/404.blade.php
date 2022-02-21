dfb9c2aae6dd17641e58808370e5d2866fc73027
Rhys Huxley <rhyshuxley@gmail.com>
2022-02-21 14:02:58 +0000

    Add 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.errorApp')
+
+@section('content')
+    <div class="content">
+        <a href="/">
+            <img src="{{ url('images/logo/logo2.png') }}" class="logo"/>
+        </a>
+        <h4>Uh oh! We couldn't find that page</h4>
+    </div>
+@endsection

5ac713abd956315bb69afab177539e798e50176f
Rhys Huxley <rhyshuxley@gmail.com>
2022-02-21 16:28:06 +0000

    Add gear upload controller

---
+++ app/Http/Controllers/Gear/UploadController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Gear;
+
+use App\Http\Controllers\Controller;
+use App\Models\Gear\Gear;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\View\View;
+use Image;
+
+class UploadController extends Controller
+{
+    protected $location;
+
+    public function __construct()
+    {
+        $this->location = 'images/gear';
+    }
+
+    public function index(): View
+    {
+        return view('gear.upload');
+    }
+
+    public function upload(Request $request): RedirectResponse
+    {
+        if ($request->image === null) {
+            return redirect()->back()->with('errors', 'No file selected');
+        }
+
+        $gear = Gear::create([
+            'name' => $request->name,
+            'order' => $request->order,
+        ]);
+
+        $image = Image::make($request->file('image'))
+            ->resize(740, null, function ($constraint) {
+                $constraint->aspectRatio();
+            })
+            ->encode('jpg', 90);
+
+        $name = $request->image->getClientOriginalName();
+
+        $path = $this->location . '/' . $name;
+
+        Storage::disk('public')->put(
+            $path,
+            $image,
+        );
+
+        $photo = Auth::user()->photos()->create([
+            'filename' => $name,
+            'folder' => $request->folder,
+            'uri' => $path,
+            'height' => $image->height(),
+            'width' => $image->width(),
+        ]);
+
+        $gear->photo()->sync($photo);
+
+        return redirect()->back()->with('success', $request->name . ' added successfully');
+    }
+}
